8d701a8bdf15b01e7e10fb05567fd7b6ba227150
shunta <exec.apea@gmail.com>
2019-11-14 08:51:57 +0900

    Add: bluma

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,8 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
 
 # Create your models here.
 
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    """カテゴリー"""
+
+    name = models.CharField('Category Name', max_length=255)
+    created_at = models.DateTimeField('created_date', default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    """Articles"""
+
+    title = models.CharField('Title', max_length=255)
+    text = models.TextField('Content')
+    created_at = models.DateTimeField('Created at', default=timezone.now)
+    image = models.ImageField(upload_to='images', default='blog/img/card1.jpg')
+    category = models.ForeignKey(Category, verbose_name='Category', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import Post
 # Create your views here.
 
 
-class IndexView(generic.TemplateView):
-    template_name = 'blog/blog_list.html'
-# Create your views here.
+class IndexView(generic.ListView):
+    model = Post  # make html file name as model name + list.html
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created_at')  # 新しい投稿順
