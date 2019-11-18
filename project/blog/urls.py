a183b5f2a322c07b21da541d76a64c7b8d4871f7
shunta <exec.apea@gmail.com>
2019-11-19 08:55:19 +0900

    Update

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('category/<int:pk>/', views.CategoryView.as_view(), name='category'),
+
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
-from .models import Post
+from .models import Post, Category
+
+
 # Create your views here.
 
 
 class IndexView(generic.ListView):
     model = Post  # make html file name as model name + list.html
-    paginate_by = 5
+    paginate_by = 7
 
     def get_queryset(self):
-        return Post.objects.all().order_by('-created_at')  # 新しい投稿順
+        queryset = Post.objects.all().order_by('-created_at')  # 新しい投稿順
+        keyword = self.request.GET.get('keyword')
+        if keyword:
+            queryset = queryset.filter(
+                Q(title__icontains=keyword) | Q(text__icontains=keyword)
+
+            )
+        return queryset
+
+
+class CategoryView(generic.ListView):
+    model = Post
+    paginate_by = 7
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        queryset = Post.objects.all().order_by('-created_at')
+        queryset = queryset.filter(category=category)
+        return queryset
